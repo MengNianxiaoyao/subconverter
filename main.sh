3cab11a91132891a745d7f84d3ead3fda1b0b92b
梦念逍遥 <103258286+MengNianxiaoyao@users.noreply.github.com>
2023-10-13 21:03:21 +0800

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+rm -rf subconverter subconverter_linux64.tar.gz
+wget -N https://github.com/tindy2013/subconverter/releases/latest/download/subconverter_linux64.tar.gz
+tar -xzvf subconverter_linux64.tar.gz
+
+cat <<EOF > /tmp/keepalive.sh
+#!/bin/bash
+
+while true; do
+  curl "https://${REPL_SLUG}.${REPL_OWNER}.repl.co" --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
+  sleep 60
+done
+EOF
+
+nohup bash /tmp/keepalive.sh > /tmp/log.out 2>&1 &  
+cd subconverter
+chmod 777 subconverter
+./subconverter 2>/dev/null
